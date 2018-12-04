cc0a10206bfbaca900907de061bd6f28b43803a6
Seek <seek@Seeks-MacBook-Pro.local>
2018-12-04 10:42:38 -0700

    initial project commit

---
+++ Mesh/app/src/main/java/com/mesh/mesh/ActivityBottomNav.kt
@@ -0,0 +1,39 @@
+package com.mesh.mesh
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
+
+class ActivityBottomNav : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                currentFragBackground.setImageResource(R.drawable.backgrounds_maps)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                currentFragBackground.setImageResource(R.drawable.background_groups)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                currentFragBackground.setImageResource(R.drawable.background_messages)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        ivSettings.setOnClickListener {
+            val intent = Intent(this@ActivityBottomNav, ActivityBottomNav::class.java)
+            startActivity(intent)
+        }
+    }
+}
